42fada809b3abef04fd169eafe28faede6861d8e
dombarrett <dominic.barrett@nyu.edu>
2018-03-01 15:52:41 -0500

    keep on working etc

--- rpiNodeSender_w_ADC-OSC.js
+++ rpiNodeSender_w_ADC-OSC.js
@@ -61,18 +61,18 @@ function readADC(){
 
       if(data<1000){
         ledRed.writeSync(0);
-        ledBlue.writeSync(0);
+        ledBlue.writeSync(1);
         ledYellow.writeSync(1);
       }
       else if(data>2000){
-        ledRed.writeSync(0);
-        ledBlue.writeSync(1);
-        ledYellow.writeSync(0);
-      }
-      else{
         ledRed.writeSync(1);
         ledBlue.writeSync(0);
         ledYellow.writeSync(0);
+      }
+      else{
+        ledRed.writeSync(0);
+        ledBlue.writeSync(0);
+        ledYellow.writeSync(1);
       }
 
       //);
